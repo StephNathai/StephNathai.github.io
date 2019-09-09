99d611f6b8ddc0a3b3119d2927fa9b401f33f042
stephnathai <spontandom@gmail.com>
2019-09-09 15:23:44 -0400

    fixing master

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Name from './Name';
+import Navigation from './Navigation';
+import './header.scss';
+
+const links = ['Home', 'Resume', 'Portfolio'];
+
+function Header({updateContent}) {
+  return (
+    <div className='header'>
+      <Name name='Stephanie Nathai-Cueter'/>
+      <hr />
+      <Navigation links={links} updateContent={updateContent}/>
+    </div>
+  );
+}
+
+export default Header;
